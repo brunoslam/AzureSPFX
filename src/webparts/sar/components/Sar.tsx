d39343aca8a749f91377956279d7a430b7b7b616
Bruno Palma Ávila <bpalma@LATINSHARE.COM>
2018-08-13 18:38:24 -0300

    asdsd

--- src/webparts/sar/components/Sar.tsx
+++ src/webparts/sar/components/Sar.tsx
@@ -8,58 +8,47 @@ import { HttpClient, SPHttpClient, HttpClientConfiguration, HttpClientResponse, 
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
-import { PivotItem, IPivotItemProps, Pivot, TextField} from 'office-ui-fabric-react';
+import { Dialog, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
+import { PivotItem, IPivotItemProps, Pivot, TextField } from 'office-ui-fabric-react';
 import { DatePicker, DayOfWeek, IDatePickerStrings } from 'office-ui-fabric-react/lib/DatePicker';
 import { PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import axios, { AxiosRequestConfig, AxiosPromise, AxiosResponse } from 'axios';
-
-
 var urlSharedKey = 'https://storagelatintest.table.core.windows.net/Persona?st=2018-08-05T22%3A14%3A19Z&se=2018-08-20T22%3A14%3A00Z&sp=raud&sv=2018-03-28&tn=persona&sig=DxU3OGGkO092uET0JPt%2FWdZRmo2Cp3%2FSyCjXcLpP3yY%3D';
 var urlAzureFunction = "https://miindicadorapi.azurewebsites.net/api/HttpTriggerJS1?code=HNrWahearYSovl/hZorLwdCmav1uz0eswO5BamXcYvsMHq15Kh5ulg==";
-import 'office-ui-fabric/dist/components/DatePicker/DatePicker.min.css';
-import 'office-ui-fabric/dist/components/DatePicker/DatePicker.min.css';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons'
+import './style.css';
 export interface estados {
   resultados: Array<any>;
-  indicadoresDelDia : any;
+  indicadoresDelDia: any;
   fechaSeleccionada: any;
+  hideDialog: boolean;
 }
 export default class Sar extends React.Component<ISarProps, estados> {
-  
-  constructor(){
+
+  constructor() {
     super();
     SPComponentLoader.loadCss('//dev.office.com/fabric-js/css/fabric.components.min.css');
 
     this.state = {
-      resultados : [],
+      resultados: [],
       indicadoresDelDia: null,
-      fechaSeleccionada: null
+      fechaSeleccionada: null,
+      hideDialog: true
     }
   }
-  
-  public componentWillMount(){
-    this.getElements().then((response) => {
-      console.log(response.data);
-      this.setState({
-        resultados : response.data.value
-      });
-      this.addElement = this.addElement.bind(this);
-    }).catch((err)=>{
-        console.log(err);
-    });
-    this.getAzureFunction().then((resp)=>{
-      this.setState({
-        indicadoresDelDia : resp
-      })
 
-    })
-    
+  public componentWillMount() {
+    this.addElementAzureTable = this.addElementAzureTable.bind(this);
+    //Obtener tabla Persona
+    this.getElementsAzureTable();
+    //Obtener MiIndicador Azure Function
+    this.getAzureFunction()
   }
   public render(): React.ReactElement<ISarProps> {
     return (
       <div>
         <Pivot >
-          <PivotItem linkText="Ver elementos" itemCount={this.state.resultados.length}  itemIcon="Emoji2">
+          <PivotItem linkText="Ver elementos" itemCount={this.state.resultados.length} itemIcon="Emoji2">
             <Label>Total Asistentes Charla SPFX:  {this.state.resultados.length}</Label>
             <table className="ms-Table">
               <thead>
@@ -74,39 +63,37 @@ export default class Sar extends React.Component<ISarProps, estados> {
               </thead>
               <tbody>
                 {
-                  this.state.resultados.map((value, key )=>{
+                  this.state.resultados.map((value, key) => {
 
                     return <tr>
-                            <td>{(key+1)}</td>
-                            <td>{value.Nombre}</td>
-                            <td>{value.Comuna}</td>
-                            <td>{ this._formatDate(value.FechaConfirmacion)}</td>
-                            <td>{value.Telefono}</td>
-                            <td>{value.Confirma ? <i className="ms-Icon ms-Icon--Emoji" aria-hidden="true"></i> : <i className="ms-Icon ms-Icon--EmojiDisappointed" aria-hidden="true"></i>}</td>
-                          </tr>
+                      <td>{(key + 1)}</td>
+                      <td>{value.Nombre}</td>
+                      <td>{value.Comuna}</td>
+                      <td>{this._formatDate(value.FechaConfirmacion)}</td>
+                      <td>{value.Telefono}</td>
+                      <td>{value.Confirma ? <i className="ms-Icon ms-Icon--Emoji" aria-hidden="true"></i> : <i className="ms-Icon ms-Icon--EmojiDisappointed" aria-hidden="true"></i>}</td>
+                    </tr>
                   })
                 }
               </tbody>
             </table>
           </PivotItem>
           <PivotItem linkText="Insertar elementos" itemIcon="Recent">
-            <Label>Pivot #2</Label>
+            <Label>Confirmar asistencia: </Label>
             <div>
               <TextField label="Nombre:" name="Nombre" id="txtNombre" />
-              <TextField label="Comuna:" name="Comuna" id="txtComuna"/>
+              <TextField label="Comuna:" name="Comuna" id="txtComuna" />
               <DatePicker
                 //firstDayOfWeek={firstDayOfWeek}
                 //strings={DayPickerStrings}
                 placeholder="Seleccionar fecha confirmación..."
                 showMonthPickerAsOverlay={true}
-                // tslint:disable:jsx-no-lambda
-                onAfterMenuDismiss={() => console.log('onAfterMenuDismiss called')}
                 // tslint:enable:jsx-no-lambda
                 label="Fecha confirmación:"
                 className="txtFechaConfirmacion"
                 formatDate={this._onFormatDate}
               />
-              <TextField label="Teléfono:" name="Telefono" id="txtTelefono"/>
+              <TextField label="Teléfono:" name="Telefono" id="txtTelefono" />
               <Toggle
                 defaultChecked={false}
                 label="Confirma asistencia:"
@@ -114,11 +101,11 @@ export default class Sar extends React.Component<ISarProps, estados> {
                 offText="No"
                 id="chkAsistencia"
               />
-              <PrimaryButton onClick={this.addElement}>Enviar confirmación <i className="ms-Icon ms-Icon--Emoji" aria-hidden="true"></i></PrimaryButton>
+              <PrimaryButton className="ms-fontColor-white" onClick={this.addElementAzureTable}>Enviar confirmación <i className="ms-Icon ms-Icon--Emoji" aria-hidden="true"></i></PrimaryButton>
             </div>
           </PivotItem>
           <PivotItem linkText="Azure API" itemIcon="Globe">
-            <Label>Fecha información: {this.state.indicadoresDelDia == null ? "" : this.state.indicadoresDelDia.fecha }</Label>
+            <Label>Fecha información: {this.state.indicadoresDelDia == null ? "" : this._formatDate(this.state.indicadoresDelDia.fecha)}</Label>
             <table className="ms-Table">
               <thead>
                 <tr>
@@ -127,64 +114,97 @@ export default class Sar extends React.Component<ISarProps, estados> {
                 </tr>
               </thead>
               {(() => {
-                if(this.state.indicadoresDelDia){
+                if (this.state.indicadoresDelDia) {
                   return <tbody>
-                          <tr>
-                            <td>{ this.state.indicadoresDelDia.dolar.nombre }</td>
-                            <td>{ this.state.indicadoresDelDia.dolar.valor }</td>
-                          </tr>
-                          <tr>
-                            <td>{ this.state.indicadoresDelDia.euro.nombre }</td>
-                            <td>{ this.state.indicadoresDelDia.euro.valor }</td>
-                          </tr>
-                          <tr>
-                            <td>{ this.state.indicadoresDelDia.bitcoin.nombre }</td>
-                            <td>{ (this.state.indicadoresDelDia.bitcoin.valor * this.state.indicadoresDelDia.dolar.valor) }</td>
-                          </tr>
-                          <tr>
-                            <td>{ this.state.indicadoresDelDia.uf.nombre }</td>
-                            <td>{ this.state.indicadoresDelDia.uf.valor }</td>
-                          </tr>
-                          <tr>
-                            <td>{ this.state.indicadoresDelDia.utm.nombre }</td>
-                            <td>{ this.state.indicadoresDelDia.utm.valor }</td>
-                          </tr>
-                        </tbody>
+                    <tr>
+                      <td>{this.state.indicadoresDelDia.dolar.nombre}</td>
+                      <td>{this.state.indicadoresDelDia.dolar.valor}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.state.indicadoresDelDia.euro.nombre}</td>
+                      <td>{this.state.indicadoresDelDia.euro.valor}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.state.indicadoresDelDia.bitcoin.nombre}</td>
+                      <td>{(this.state.indicadoresDelDia.bitcoin.valor * this.state.indicadoresDelDia.dolar.valor)}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.state.indicadoresDelDia.uf.nombre}</td>
+                      <td>{this.state.indicadoresDelDia.uf.valor}</td>
+                    </tr>
+                    <tr>
+                      <td>{this.state.indicadoresDelDia.utm.nombre}</td>
+                      <td>{this.state.indicadoresDelDia.utm.valor}</td>
+                    </tr>
+                  </tbody>
                 }
               })()}
-              
+
             </table>
           </PivotItem>
         </Pivot>
+        <Dialog
+          hidden={this.state.hideDialog}
+          onDismiss={this._closeDialog}
+          dialogContentProps={{
+            type: DialogType.normal,
+            title: 'Registro exitoso',
+            subText:
+              'Se confirmado correctamente al usuario.'
+          }}
+          modalProps={{
+            titleAriaId: 'myLabelId',
+            subtitleAriaId: 'mySubTextId',
+            isBlocking: false,
+            containerClassName: 'ms-dialogMainOverride'
+          }}
+        >
+        </Dialog>
       </div>
     );
   }
-  private _customRenderer(link: IPivotItemProps, defaultRenderer: (link: IPivotItemProps) => JSX.Element): JSX.Element {
-    return (
-      <span>
-        {defaultRenderer(link)}
-        <Icon iconName="Airplane" style={{ color: 'red' }} />
-      </span>
-    );
-  }
+  private _showDialog = (): void => {
+    this.setState({ hideDialog: false });
+  };
+  private _closeDialog = (): void => {
+    this.setState({ hideDialog: true });
+  };
   private _onFormatDate = (date: Date): string => {
-    debugger;
-    this.setState({fechaSeleccionada : date});
+    this.setState({ fechaSeleccionada: date });
     return date.getDate() + '/' + (date.getMonth() + 1) + '/' + (date.getFullYear() % 100);
   };
-
-  private _formatDate(date){
-    var newDate = date == "" ||  date == null ? "" : new Date(date).toLocaleDateString();
+  private _formatDate(date) {
+    var newDate = date == "" || date == null ? "" : new Date(date).toLocaleDateString();
     return newDate;
   }
-  private addElement(){
-    
-    var nombre = (document.getElementById("txtNombre")  as HTMLInputElement).value;
-    var comuna = (document.getElementById("txtComuna")  as HTMLInputElement).value;
+
+  
+  private getElementsAzureTable() {
+    this.getAzureFunction();
+    const tablestorageUrl = urlSharedKey;
+    return axios.get(tablestorageUrl, {
+      headers: {
+        Accepts: 'application/json'
+      }
+    }).then((response) => {
+      console.log(response.data);
+      this.setState({
+        resultados: response.data.value
+      });
+     
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  private addElementAzureTable() {
+    var ctx = this;
+    var nombre = (document.getElementById("txtNombre") as HTMLInputElement).value;
+    var comuna = (document.getElementById("txtComuna") as HTMLInputElement).value;
     //var fechaConfirmacion = (document.querySelector(".txtFechaConfirmacion input[type=text]")  as HTMLInputElement).value;
-    var fechaConfirmacion = this.state.fechaSeleccionada;
-    var telefono = (document.getElementById("txtTelefono")  as HTMLInputElement).value;
-    var confirmacion = (document.getElementById("chkAsistencia")  as HTMLInputElement).getAttribute("aria-pressed") == "true" ? true : false;
+    var fechaConfirmacion = ctx.state.fechaSeleccionada;
+    var telefono = (document.getElementById("txtTelefono") as HTMLInputElement).value;
+    var confirmacion = (document.getElementById("chkAsistencia") as HTMLInputElement).getAttribute("aria-pressed") == "true" ? true : false;
     const tablestorageUrl = urlSharedKey;
     axios.post(tablestorageUrl, {
       "PartitionKey": new Date(),
@@ -193,55 +213,46 @@ export default class Sar extends React.Component<ISarProps, estados> {
       "Comuna": comuna,
       "FechaConfirmacion": fechaConfirmacion,
       "Telefono": telefono,
-      "Confirma" : confirmacion
-            
+      "Confirma": confirmacion
     }, {
-      url: tablestorageUrl,
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(function (response) {
-      debugger;
-      console.log(response);
-    })
-    .catch(function (error) {
-      debugger;
-      console.log(error);
-    });
-  
-    
-  }
-  private getElements()  {
-    this.getAzureFunction();
-    const tablestorageUrl = urlSharedKey;
-    return axios.get(tablestorageUrl, {
-      headers: {
-        Accepts: 'application/json'
-      }});
+        url: tablestorageUrl,
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(function (response) {
+        ctx._showDialog();
+        ctx.getAzureFunction();
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
   }
 
-  private getAzureFunction():Promise<any>{
+  private getAzureFunction(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       const requestHeaders: Headers = new Headers();
       requestHeaders.append("Content-type", "application/jsonp");
       requestHeaders.append("Cache-Control", "no-cache");
       const postOptions: IHttpClientOptions = {
         headers: requestHeaders
-        //,credentials: "include"
       };
 
       this.props.context_.httpClient.get(urlAzureFunction, HttpClient.configurations.v1, postOptions).then((response: HttpClientResponse) => {
         response.json().then((resp: string) => {
-            var responseText = resp != null ? JSON.parse(resp) : null;
-            resolve(responseText);
-          })
-          .catch ((response: any) => {
+          var responseText = resp != null ? JSON.parse(resp) : null;
+          this.setState({
+            indicadoresDelDia: responseText
+          });
+          resolve(responseText);
+        })
+          .catch((response: any) => {
             reject(response);
-        });
+          });
       });
     });
-
-   
   }
 }
